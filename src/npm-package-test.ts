aa0d7c2abf36326844c13ffadb60cb0569d11b5e
jeldikk <jeldi.kamal2013@gmail.com>
2021-05-24 18:27:51 +0530

    minimal changes

--- src/npm-package-test.ts
+++ src/npm-package-test.ts
@@ -1,11 +1,11 @@
-export function add(a: number, b: number): number {
+export function add(a: number, b: number){
     return a+b;
 }
 
-export function multiply(a: number, b: number): number {
+export function multiply(a: number, b: number){
     return a*b;
 }
 
-export function power(a: number, n: number): number {
+export function power(a: number, n: number){
     return a**n;
 }
